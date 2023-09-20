ec8272f03a88e95cccbba7443648f982fc4d9303
faisalrehman <faisalfasi18@gmail.com>
2023-09-21 01:00:08 +0200

    pages, component and basic layout with style added

---
+++ src/app/blog/[id]/page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./page.module.css";
+
+const BlogPost = () => {
+  return <div className={styles.container}>Blog Post</div>;
+};
+
+export default BlogPost;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./footer.module.css";
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div>© 2023 FR-Blogs. All Rights Reserved.</div>
+    </div>
+  );
+};
+
+export default Footer;
